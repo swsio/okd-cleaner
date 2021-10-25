c7e8a66e28f5face31e79725bee0902edf531f96
Florian Fröhlich <44257732+ffsws@users.noreply.github.com>
2021-10-25 09:21:44 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh 
 export KUBECONFIG=/tmp/.kube/config
 oc login --token=`cat /var/run/secrets/kubernetes.io/serviceaccount/token` https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT --insecure-skip-tls-verify
-oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' > /tmp/csrs
-LINECOUNT=`wc -l /tmp/csrs | awk '{print $1}'`
+oc delete pod --field-selector=status.phase==Failed --all-namespaces > /tmp/pods
+LINECOUNT=`wc -l /tmp/pods | awk '{print $1}'`
 if [ $LINECOUNT -gt 0 ]; then
-    cat /tmp/csrs | xargs oc adm certificate approve
+    echo "Cleaned Pods!"
 else
-    echo "No CSRs found, nothing to do!"
+    echo "No Pods found, nothing to do!"
 fi
