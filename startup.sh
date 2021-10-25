84c258b82fd38e0e74afe458ca54093e84906030
Florian Fröhlich <44257732+ffsws@users.noreply.github.com>
2021-10-25 09:46:27 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ export KUBECONFIG=/tmp/.kube/config
 oc login --token=`cat /var/run/secrets/kubernetes.io/serviceaccount/token` https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT --insecure-skip-tls-verify
 oc delete pod --field-selector=status.phase==Failed --all-namespaces > /tmp/pods
 LINECOUNT=`wc -l /tmp/pods | awk '{print $1}'`
-if [ $LINECOUNT -gt 0 ]; then
+if [ $LINECOUNT -gt 1 ]; then
     echo "Cleaned Pods!"
 else
     echo "No Pods found, nothing to do!"
