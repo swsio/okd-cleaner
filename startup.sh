71809b4c82fb920a8487b2b79192cc2a5fa50354
Florian Fröhlich <44257732+ffsws@users.noreply.github.com>
2022-01-05 11:23:11 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,9 +4,15 @@ touch /tmp/.kube/config
 export KUBECONFIG=/tmp/.kube/config
 oc login --token=`cat /var/run/secrets/kubernetes.io/serviceaccount/token` https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT --insecure-skip-tls-verify
 oc delete pod --field-selector=status.phase==Failed --all-namespaces > /tmp/pods
+oc delete job -n openshift-logging $(oc get job -n openshift-logging -o=jsonpath='{.items[?(@.status.failed==1)].metadata.name}') > /tmp/jobs
 LINECOUNT=`wc -l /tmp/pods | awk '{print $1}'`
+LINECOUNTJOBS=`wc -l /tmp/jobs | awk '{print $1}'`
 if [ $LINECOUNT -gt 1 ]; then
     echo "Cleaned Pods!"
 else
     echo "No Pods found, nothing to do!"
+if [ $LINECOUNTJOBS -gt 1 ]; then
+    echo "Cleaned Jobs!"
+else
+    echo "No Jobs found, nothing to do!"
 fi
